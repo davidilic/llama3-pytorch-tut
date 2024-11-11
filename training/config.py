2efd3f9252baccd832e61fce1ebc2a6602db353e
davidilic <davidilic044@gmail.com>
2024-11-11 18:59:58 +0100

    fixed checkpointing bug

--- training/config.py
+++ training/config.py
@@ -7,7 +7,7 @@ class TrainingConfig:
     max_learning_rate: float = 1e-1
     num_epochs: int = 10
     patience: int = 3
-    checkpoint_path: Path = Path('checkpoint.pth')
+    checkpoints_folder: Path = Path('checkpoints')
     dataset_path: str = "data/pes2o"
     batch_size: int = 8
     context_length: int = 1024
